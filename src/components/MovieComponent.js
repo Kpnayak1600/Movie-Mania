53a61a53e22b71088f1a6322722cfb5a8360f018
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-09-12 13:15:08 +0530

    logic modified to display content by default

--- src/components/MovieComponent.js
+++ src/components/MovieComponent.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { MovieContainer, CoverImage, MovieName, InfoColumn, MovieInfo } from './MovieComponent.styled';
 
-const MovieComponent = (props) => {
-  const { Title, Year, imdbID, Type, Poster } = props.movie;
+const MovieComponent = ({ movie, updateSelectedMovie }) => {
+  const { Title, Year, imdbID, Type, Poster } = movie;
 
   return (
     <MovieContainer
       onClick={() => {
-        props.onMovieSelect(imdbID);
+        // when clicked on any movie then it to be selected in order to get more info about this movie
+        updateSelectedMovie(imdbID);
+        // scrolls the window to the top of the page with a smooth scrolling animation. 
+        // window.scrollTo method, where { top: 0, behavior: "smooth" } specifies that 
+        //it should scroll to the top of the page (top: 0) with a smooth animation (behavior: "smooth").
         window.scrollTo({ top: 0, behavior: "smooth" });
       }}
     >
--- src/components/MovieInfoComponent.js
+++ src/components/MovieInfoComponent.js
@@ -3,19 +3,40 @@ import Axios from "axios";
 import { API_KEY } from "../pages/Home";
 import { Container, CoverImage, InfoColumn, MovieInfo, MovieName, Close } from './MovieInfoComponent.styled';
 
-const MovieInfoComponent = (props) => {
+const MovieInfoComponent = ({ selectedMovie, updateSelectedMovie }) => {
+  // movieInfo state store information about the selected movie.
   const [movieInfo, setMovieInfo] = useState();
-  const { selectedMovie } = props;
-
+  //useEffect is a React hook that takes two arguments: a function and an array of dependencies.
   useEffect(() => {
+    // functional argument :  API request is made, and when the response is received, the response.data is 
+    //                   used to update the movieInfo state variable using setMovieInfo. This causes a 
+    //                  re-render of the component with the fetched movie information, and the UI updates accordingly
     Axios.get(
       `https://www.omdbapi.com/?i=${selectedMovie}&apikey=${API_KEY}`,
     ).then((response) => setMovieInfo(response.data));
-  }, [selectedMovie]);
+  },
+    // array argument : It specifies when the effect should run. In your code, [selectedMovie] is provided as 
+    //                  the dependency array. This means that the effect will run whenever the selectedMovie prop 
+    //                  changes. When selectedMovie changes (i.e., when a new movie is selected), the effect will 
+    //                  trigger, making a new API request.
+    [selectedMovie]
+  );
   return (
     <Container>
+      {/* conditional rendering -> movieInfo ? jsx : Loading
+          If movieInfo is truthy (i.e., it contains data), the first block of JSX inside the parentheses 
+          is rendered. If movieInfo is falsy (e.g., null or undefined), the second block of JSX is rendered.
+      */}
       {movieInfo ? (
         <>
+          {/* movieInfo.?object_Property? -> we can also write movieInfo.object_Property 
+              .? -> called optional chaining operator  introduced in JavaScript to help avoid errors 
+                    that occur when trying to access properties or methods on null or undefined values.
+                    Example : => movieInfo?.Poster: This code attempts to access the Poster property of 
+                                 the movieInfo object. If movieInfo is null or undefined, this expression 
+                                 will not throw an error; instead, it will return undefined. If movieInfo 
+                                 is an object with a Poster property, it will return the value of that property       
+          */}
           <CoverImage src={movieInfo?.Poster} alt={movieInfo?.Title} />
           <InfoColumn>
             <MovieName>
@@ -52,7 +73,9 @@ const MovieInfoComponent = (props) => {
               Plot: <span>{movieInfo?.Plot}</span>
             </MovieInfo>
           </InfoColumn>
-          <Close onClick={() => props.onMovieSelect()}>X</Close>
+          {/* when clik on X ,we need to close the info of selected movie so we will update useState with empty
+              and for this we will use updateSelectedMovie()*/}
+          <Close onClick={() => updateSelectedMovie()}>X</Close>
         </>
       ) : (
         "Loading..."
